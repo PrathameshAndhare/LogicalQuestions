b981ca8c578d1f1aeec089dd06c8b30cd9ac9392
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2022-09-08 22:10:17 +0530

    Add files via upload

--- 12.js
+++ 12.js
@@ -51,49 +51,49 @@
 // //a....z
 // // A...Z
 
-// let sss=[1,1,3,4,3,3,2,2,23,6,6,5]
-// let m=new Map()
-// for(let i=0;i<sss.length;i++){
-//     m.set(sss[i],0)
-// }
-// // console.log(mm)
-// for(let i=0;i<sss.length;i++){
-//     let r=m.get(sss[i])
-//     r++
-//     m.set(sss[i],r)
-// }
-// console.log(m)
-
-
-// let arr=['abc','xyz','qwe','abc','abc','abc','xyz']
-// let mm=new Map()// {1,2}
-
-// for(let i=0;i<arr.length;i++){
-//     mm.set(arr[i],0)
-// }
-// // console.log(mm)
-// for(let i=0;i<arr.length;i++){
-//     let r=mm.get(arr[i])
-//     r++
-//     mm.set(arr[i],r)
-// }
+let sss=[1,1,3,4,3,3,2,2,23,6,6,5]
+let m=new Map()
+for(let i=0;i<sss.length;i++){
+    m.set(sss[i],0)
+}
 // console.log(mm)
+for(let i=0;i<sss.length;i++){
+    let r=m.get(sss[i])
+    r++
+    m.set(sss[i],r)
+}
+console.log(m)
 
-// /// .......................................
-// function alpha(a){
-//     let mm=new Map()// {1,2}
 
-//     for(let i=0;i<a.length;i++){
-//         mm.set(a[i],0)
-//     }
-//     // console.log(mm)
-//     for(let i=0;i<a.length;i++){
-//         let r=mm.get(a[i])
-//         r++
-//         mm.set(a[i],r)
-//     }
-//     console.log(mm)
+let arr=['abc','xyz','qwe','abc','abc','abc','xyz']
+let mm=new Map()// {1,2}
 
-// }
+for(let i=0;i<arr.length;i++){
+    mm.set(arr[i],0)
+}
+// console.log(mm)
+for(let i=0;i<arr.length;i++){
+    let r=mm.get(arr[i])
+    r++
+    mm.set(arr[i],r)
+}
+console.log(mm)
 
-// alpha(['a','a','a','d','dre','wer','wer','wer',12,12,12,'@','@'])
+/// .......................................
+function alpha(a){
+    let mm=new Map()// {1,2}
+
+    for(let i=0;i<a.length;i++){
+        mm.set(a[i],0)
+    }
+    // console.log(mm)
+    for(let i=0;i<a.length;i++){
+        let r=mm.get(a[i])
+        r++
+        mm.set(a[i],r)
+    }
+    console.log(mm)
+
+}
+
+alpha(['a','a','a','d','dre','wer','wer','wer',12,12,12,'@','@'])
